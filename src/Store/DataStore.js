41dd497438e4fc025476fed7c67fd5196f02254e
Prasobh-23 <prasobh.kylm96@gmail.com>
2021-12-03 23:48:55 +0530

    local storage

--- src/Store/DataStore.js
+++ src/Store/DataStore.js
@@ -1,5 +1,10 @@
 import { writable } from "svelte/store";
 
-const DataStore = writable({});
+
+const DataStore = writable(JSON.parse(localStorage.getItem("data")) || {});
+DataStore.subscribe(value => {
+    localStorage.setItem("data", JSON.stringify(value));
+    console.log(value);
+});
 
 export default DataStore;
